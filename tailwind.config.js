1bfb85e7a8ff914979737508b6f72392648158e8
Obi Nkemjika Derick <obinkemjikaderick@Obis-MacBook-Pro.local>
2022-06-15 10:12:54 +0100

    Dockerized cryp frontend, Tsconfig absolute imports and Read Me updated

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,41 @@
 module.exports = {
-  content: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  content: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}", "./src/components/**/*.{js,ts,jsx,tsx}"],
+  safelist: [
+    {
+      pattern: /bg-/,
+      variants: ["hover"],
+    },
+  ],
   theme: {
-    extend: {},
+    container: {
+      center: true,
+    },
+    screens: {
+      mobile: "640px",
+      tablet: "768px",
+      smallLaptop: "1024px",
+      desktop: "1280px",
+      bigLaptop: "1440px",
+      television: "1536px",
+    },
+    extend: {
+      fontSize: {
+        10: ["0.625rem", "0.938rem"],
+        12: ["0.75rem", "1rem"],
+        13: ["0.813rem", "1.125rem"],
+        14: ["0.875rem", "1.118rem"],
+        16: ["1rem", "1.313rem"],
+        18: ["1.125rem", "2rem"],
+        20: ["1.25rem", "1.688rem"],
+        24: ["1.5rem", "2.063rem"],
+        40: ["2.5rem", "3.375rem"],
+        64: ["4rem", "4.375rem"],
+      },
+      width: {
+        600: "600px",
+        500: "500px",
+      },
+    },
   },
   plugins: [],
 };
