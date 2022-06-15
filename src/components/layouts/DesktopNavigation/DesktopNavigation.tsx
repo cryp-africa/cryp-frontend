9ccaad2db7a3df2b693f6b2bc844467e16f9cb01
Obi Nkemjika Derick <obinkemjikaderick@Obis-MacBook-Pro.local>
2022-06-15 19:44:36 +0100

    Custom Link and Desktop Nav

--- src/components/layouts/DesktopNavigation/DesktopNavigation.tsx
+++ src/components/layouts/DesktopNavigation/DesktopNavigation.tsx
@@ -1,7 +1,15 @@
+import CustomLink from "@components/atoms/CustomLink";
+import Icon from "@components/atoms/Icons";
 import React from "react";
 
 const DesktopNavigation = () => {
-  return <div>DesktopNavigation</div>;
+  return (
+    <div className="px-16 py-16 bigLaptop:px-20">
+      <CustomLink destination="/">
+        <Icon name="logo" />
+      </CustomLink>
+    </div>
+  );
 };
 
 export default DesktopNavigation;
