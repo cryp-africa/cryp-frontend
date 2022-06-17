66cd3276a1b0edd7bc8df2a94d917266b02a32e4
Obi Nkemjika Derick <obinkemjikaderick@Obis-MacBook-Pro.local>
2022-06-17 18:23:42 +0100

    adjust line height

--- src/components/Building/Building.tsx
+++ src/components/Building/Building.tsx
@@ -5,7 +5,7 @@ import { Link } from "react-scroll";
 const Building = ({ focusOnSubscribe }: any) => {
   return (
     <div className="mt-16 absolute top-[20%]">
-      <h3 className="text-[30px] tablet:text-[40px] smallLaptop:text-[45px] desktop:text-64 tablet:w-3/4 smallLaptop:w-full tablet:font-medium">
+      <h3 className="text-[30px] leading-[3.5rem] tablet:leading-[4rem] smallLaptop:leading-[5rem] desktop:leading-[6.375rem] tablet:text-[40px] smallLaptop:text-[45px] desktop:text-[4rem] tablet:w-full  tablet:font-medium">
         Building the operating system for <span className="text-crypGreen-500 font-extrabold">African Businesses</span> on the blockchain
       </h3>
       <div className="hidden tablet:block">
