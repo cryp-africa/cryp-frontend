b5f6b0131a7b57530cca72e3325e69bf92cbde4f
Obi Nkemjika Derick <obinkemjikaderick@Obis-MacBook-Pro.local>
2022-06-17 16:46:15 +0100

    Ts particles

--- src/components/ParticlesContainer/ParticlesContainer.tsx
+++ src/components/ParticlesContainer/ParticlesContainer.tsx
@@ -70,7 +70,7 @@ const ParticlesContainer = () => {
               default: "bounce",
             },
             random: false,
-            speed: 6,
+            speed: 2,
             straight: false,
           },
           number: {
