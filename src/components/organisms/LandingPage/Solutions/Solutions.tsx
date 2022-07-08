7787814d740761acfe7ed263cfc74fde99c005fd
Obi Nkemjika Derick <obinkemjikaderick@Obis-MacBook-Pro.local>
2022-07-08 01:20:36 +0100

    Ui fixes and responsiveness

--- src/components/organisms/LandingPage/Solutions/Solutions.tsx
+++ src/components/organisms/LandingPage/Solutions/Solutions.tsx
@@ -7,7 +7,9 @@ import { SolutionsData } from "@components/componentData/SolutionsData/Solutions
 const Solutions = () => {
   return (
     <div className="mb-16 smallLaptop:mb-32 tablet:ml-0">
-      <h1 className="text-center capitalize font-semibold text-20 tablet:text-24 smallLaptop:text-[2.938rem] mb-8 smallLaptop:mb-24 leading-[3.438rem] ">our solutions for your business</h1>
+      <h1 className="text-center capitalize font-semibold text-20 tablet:text-24 smallLaptop:text-[2.938rem] mb-8 smallLaptop:mb-24 leading-[3.438rem] ">
+        our solutions for your business
+      </h1>
       <div className="flex tablet:items-center flex-col bigLaptop:flex-row justify-center w-full relative ]">
         {SolutionsData.map((solution) => (
           <div
@@ -27,10 +29,14 @@ const Solutions = () => {
                 </div>
               )}
             </div>
-            <h2 className="text-37 tablet:text-[6.25rem] font-bold text-right tablet:text-center text-crypGreen-800 uppercase mt-8 tablet:mt-16 mb-12 tablet:mb-16 smallLaptop:mb-20 desktop:mb-12">{solution.title}</h2>
+            <h2 className="text-37 tablet:text-[6.25rem] font-bold text-right tablet:text-center text-crypGreen-800 uppercase mt-8 tablet:mt-16 mb-12 tablet:mb-16 smallLaptop:mb-20 desktop:mb-12">
+              {solution.title}
+            </h2>
             <p className="uppercase text-left tablet:text-center text-16 smallLaptop:text-20 font-semibold ">{solution.subHeader}</p>
             <div className="flex items-center tablet:justify-center">
-              <p className="text-12 smallLaptop:text-16 text-white opacity-70 text-left tablet:text-center mt-12 mb-12 tablet:mb-32 w-full tablet:w-3/4 font-medium ">{solution.body}</p>
+              <p className="text-12 smallLaptop:text-16 text-white opacity-70 text-left tablet:text-center mt-12 mb-12 tablet:mb-32 w-full tablet:w-3/4 font-medium ">
+                {solution.body}
+              </p>
             </div>
             <CustomLink customClass="text-12 smallLaptop:text-16 font-semibold text-crypYellow-200 absolute bottom-7 right-5 tablet:left-10 flex items-center" destination="#">
               Explore <Icon className="ml-4" name="yellowArrowRight" />
