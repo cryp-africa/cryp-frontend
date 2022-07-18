26799072d13dcbc8d67bf82ef66c955cfc01c3db
Obi Nkemjika Derick <obinkemjikaderick@Obis-MacBook-Pro.local>
2022-07-18 14:22:55 +0100

    ui fixes

--- src/components/organisms/LandingPage/Building/Building.tsx
+++ src/components/organisms/LandingPage/Building/Building.tsx
@@ -15,7 +15,7 @@ const Building = () => {
           <span className="text-crypGreen-500 font-extrabold">African Businesses</span> on the blockchain
         </p>
         <p className="text-14 tablet:text-16 smallLaptop:text-18 text-white opacity-60 smallLaptop:opacity-70 font-medium w-full tablet:w-[55%] mt-8 smallLaptop:mt-12">
-          We help you seamlessly process business payments/collections and get access to markets beyond your borders.
+          We help you seamlessly process business payments/collections and give you access to markets beyond your borders.
         </p>
         <CustomButton
           customClass="w-[10.25rem] h-[3.75] bigLaptop:h-[5rem] bigLaptop:w-[12rem] mt-12"
