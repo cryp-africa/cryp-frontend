4837099717335d96f542bda91bc38082cbbd8aa1
Obi Nkemjika Derick <obinkemjikaderick@Obis-MacBook-Pro.local>
2022-08-04 23:29:51 +0100

    resize icons

--- src/components/organisms/LandingPage/ShowCase/ShowCase.tsx
+++ src/components/organisms/LandingPage/ShowCase/ShowCase.tsx
@@ -4,7 +4,7 @@ const ShowCase = () => {
   return (
     <div className="mb-16 smallLaptop:mb-40">
       <div className=" -ml-[50vw] left-[50%] relative w-screen">
-        <img className="" src="/images/showcase/showcase-desktop.png" />
+        <img className="" src="/images/showcase/showcase.png" />
       </div>
     </div>
   );
