b57aca62a602d68f2b2167b72002f2faa0974b48
Obi Nkemjika Derick <obinkemjikaderick@Obis-MacBook-Pro.local>
2022-08-08 23:33:22 +0100

    join waitlist web fixes

--- src/components/atoms/Icons/stock/yellowLine.tsx
+++ src/components/atoms/Icons/stock/yellowLine.tsx
@@ -5,8 +5,8 @@ import React from "react";
  * @return {React.Component} React component
  */
 const SVG = (props: any): unknown => (
-  <svg fill="none" height="3" viewBox="0 0 210 3" width="210" xmlns="http://www.w3.org/2000/svg" {...props}>
-    <rect fill="#F4CA64" height="3" rx="1.5" width="210" />
+  <svg fill="none" height="3" viewBox="0 0 214 3" width="214" xmlns="http://www.w3.org/2000/svg" {...props}>
+    <rect fill="#F4CA64" height="2.2022" rx="1.1011" width="213.974" x="0.0263672" y="0.384613" />
   </svg>
 );
 
--- src/components/organisms/modals/JoinWaitList/JoinWaitList.tsx
+++ src/components/organisms/modals/JoinWaitList/JoinWaitList.tsx
@@ -51,15 +51,15 @@ const JoinWaitList = ({ setThankYou, setJoinWaitList }: JoinWaitListProps) => {
   }
 
   return (
-    <div className="flex flex-col justify-center rounded-2xl items-center bg-waitList bg-no-repeat bg-cover bg-center">
-      <Toaster position="top-right" />
+    <div className="flex flex-col justify-center rounded-2xl items-center bg-waitList bg-no-repeat bg-cover bg-center w-[41.75rem] h-[40.5rem]">
+      <Toaster position="top-center" />
       <div className="flex items-center">
-        <Dialog.Title as="h4" className="mb-4 ml-8 tablet:ml-0 capitalize text-md tablet:text-[2.313rem] whitespace-nowrap font-semibold mt-8">
+        <Dialog.Title as="h4" className="mb-4 -ml-32  capitalize text-md tablet:text-[1.875rem] whitespace-nowrap font-semibold mt-8">
           Get Notified
         </Dialog.Title>
         <Icon className="ml-4 w-[40%] tablet:w-full tablet:ml-8 mt-6" name="yellowLine" />
       </div>
-      <p className="capitalize text-md tablet:text-[2.313rem] ml-8">
+      <p className="capitalize text-md tablet:text-[1.875rem] -ml-16">
         when <span className="text-crypYellow-200">we launch</span>
       </p>
       <div className="w-full p-8">
@@ -67,7 +67,7 @@ const JoinWaitList = ({ setThankYou, setJoinWaitList }: JoinWaitListProps) => {
           <Formik initialValues={initialState} onSubmit={handleSubmit} validationSchema={JoinWaitListSchema}>
             {(props: FormikProps<Values>) => (
               <Form>
-                <div className="mt-8">
+                <div className="w-[80%] pl-8 mt-8">
                   <FormikCustomInput
                     className="!inline-block !border-b-[1px]"
                     container="!bg-transparent text-white !px-0"
@@ -77,7 +77,7 @@ const JoinWaitList = ({ setThankYou, setJoinWaitList }: JoinWaitListProps) => {
                     type="text"
                   />
                   <FormikCustomInput
-                    className="!inline-block !border-b-[1px] mt-16"
+                    className="!inline-block !border-b-[1px] mt-12"
                     container="!bg-transparent text-white !px-0"
                     inputClassName="!bg-transparent placeholder:!font-medium !text-base placeholder:!text-gray-400"
                     name="email"
@@ -85,7 +85,7 @@ const JoinWaitList = ({ setThankYou, setJoinWaitList }: JoinWaitListProps) => {
                     type="email"
                   />
                   <FormikCustomInput
-                    className="!inline-block !border-b-[1px] mt-16"
+                    className="!inline-block !border-b-[1px] mt-12"
                     container="!bg-transparent text-white !px-0"
                     inputClassName="!bg-transparent placeholder:!font-medium !text-base placeholder:!text-gray-400"
                     name="country"
@@ -93,10 +93,9 @@ const JoinWaitList = ({ setThankYou, setJoinWaitList }: JoinWaitListProps) => {
                     type="text"
                   />
                 </div>
-
-                <div className="bg-citiBlue-200 text-center mt-16 rounded-md w-[10.25rem] tablet:w-[12rem] mx-auto">
+                <div className="bg-citiBlue-200 text-center mt-16 rounded-md w-[10.25rem] tablet:w-[8.809rem] ml-[30%]">
                   <CustomButton
-                    customClass="!w-full font-14 rounded-md uppercase h-[3.75rem] tablet:h-[5rem]"
+                    customClass="!w-full font-14 rounded-md uppercase h-[3.75rem] tablet:h-[3.671rem]"
                     handleClick={() => {}}
                     isDisabled={loading}
                     isSubmitting={loading}
