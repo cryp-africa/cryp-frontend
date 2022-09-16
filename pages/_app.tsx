2eef0feca4ef80f3b173daddf8b84994ed42a9ad
Obi Nkemjika Derick <obinkemjikaderick@Obis-MacBook-Pro.local>
2022-09-16 11:06:03 +0100

    google anayltics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,6 @@
 import type { AppProps } from "next/app";
 import Router from "next/router";
 import Script from "next/script";
-// import { GoogleAnalytics } from "nextjs-google-analytics";
 import NProgress from "nprogress";
 import React from "react";
 
@@ -25,19 +24,8 @@ const MyApp = ({ Component, pageProps }: AppProps) => {
   Router.events.on("routeChangeComplete", () => NProgress.done());
   Router.events.on("routeChangeError", () => NProgress.done());
 
-  //   <!-- Google tag (gtag.js) -->
-  // <script async src="https://www.googletagmanager.com/gtag/js?id=G-4V2QZ9EVWQ"></script>
-  // <script>
-  //   window.dataLayer = window.dataLayer || [];
-  //   function gtag(){dataLayer.push(arguments);}
-  //   gtag('js', new Date());
-
-  //   gtag('config', 'G-4V2QZ9EVWQ');
-  // </script>
-
   return (
     <>
-      {/* <GoogleAnalytics trackPageViews /> */}
       <Script src="https://www.googletagmanager.com/gtag/js?id=G-4V2QZ9EVWQ" strategy="lazyOnload" />
       <Script strategy="lazyOnload">
         {`
