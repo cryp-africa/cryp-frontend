5acfa39d2cfe13ed52dac4cf2154809774fc5f3e
Obi Nkemjika Derick <obinkemjikaderick@Obis-MacBook-Pro.local>
2022-11-27 01:23:17 +0100

    revert preloader

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,3 @@
-import imagesLoaded from "imagesloaded";
 import type { AppProps } from "next/app";
 import Router from "next/router";
 import Script from "next/script";
@@ -8,7 +7,6 @@ import React from "react";
 import "../styles/globals.css";
 
 import "nprogress/nprogress.css";
-import LoadingScreen from "@components/atoms/LoadingScreen/LoadingScreen";
 
 /**
  * App wrapper for the whole application
@@ -25,21 +23,6 @@ const MyApp = ({ Component, pageProps }: AppProps) => {
   Router.events.on("routeChangeStart", () => NProgress.start());
   Router.events.on("routeChangeComplete", () => NProgress.done());
   Router.events.on("routeChangeError", () => NProgress.done());
-  const [loading, setLoading] = React.useState(true);
-
-  React.useEffect(() => {
-    if (typeof window !== "undefined") {
-      fetchImages();
-    }
-  });
-
-  const fetchImages = async () => {
-    const images = document.querySelectorAll("img");
-    console.log(images, "images1");
-    if (typeof window !== "undefined" && images) {
-      imagesLoaded(document.querySelectorAll("img"), { background: true }, (instance) => setLoading(false));
-    }
-  };
 
   return (
     <>
@@ -49,11 +32,9 @@ const MyApp = ({ Component, pageProps }: AppProps) => {
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
-
             gtag('config', 'G-4V2QZ9EVWQ');
           `}
       </Script>
-      <LoadingScreen loading={loading} />
       <Component {...pageProps} />
     </>
   );
