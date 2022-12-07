9a12e48d61a790438e8151542b8329c3952c6dbb
Obi Nkemjika Derick <obinkemjikaderick@Obis-MacBook-Pro.local>
2022-12-07 16:01:00 +0100

    statichomedata

---
+++ lib/staticHomeData.ts
@@ -0,0 +1,113 @@
+/**
+ *
+ * @return {any}
+ */
+export function getHomeData() {
+  const bgContent = {
+    downsizeMobile: "/images/downsize-mobile.png",
+    downsizeDesktop: "/images/downsize-desktop.png",
+  };
+
+  const showCaseContent = "/images/showcase/showcase.png";
+
+  const nftContent = [
+    {
+      id: 1,
+      name: "Moerieda Mackay",
+      lastName: "Mackay",
+      position: "Founder & CEO of Stride",
+      image: "/images/advisors/nft1.png",
+      about: {
+        name: "Moerieda Mackay",
+        title: "Pioneer Member of The Blockchain Research Institute",
+        body: "Kogi salvia cillum selvage small batch aliquip seitan DSA bitters. Ad chambray occaecat poutine esse humblebrag XOXO authentic hot chicken.",
+      },
+    },
+    {
+      id: 2,
+      name: "Ali Hussein Kassim",
+      lastName: "Kassim",
+      position: "Chairman, Association of Fintechs, Kenya",
+      image: "/images/advisors/nft2.png",
+      about: {
+        name: "Ali Hussein Kassim",
+        title: "Pioneer Member of The Blockchain Research Institute",
+        body: "Kogi salvia cillum selvage small batch aliquip seitan DSA bitters. Ad chambray occaecat poutine esse humblebrag XOXO authentic hot chicken.",
+      },
+    },
+    {
+      id: 3,
+      name: "Alakanani Itireleng",
+      lastName: "Itireleng",
+      position: "CEO, Satoshicentre",
+      image: "/images/advisors/nft3.png",
+      about: {
+        name: "Alakanani Itireleng",
+        title: "Pioneer Member of The Blockchain Research Institute",
+        body: "Kogi salvia cillum selvage small batch aliquip seitan DSA bitters. Ad chambray occaecat poutine esse humblebrag XOXO authentic hot chicken.",
+      },
+    },
+    {
+      id: 4,
+      name: "Raj A. Kapoor",
+      lastName: "Kapoor",
+      position: "Founder/CEO India Blockchain Alliance",
+      image: "/images/advisors/nft4.png",
+      about: {
+        name: "Raj A. Kapoor",
+        title: "Pioneer Member of The Blockchain Research Institute",
+        body: "Kogi salvia cillum selvage small batch aliquip seitan DSA bitters. Ad chambray occaecat poutine esse humblebrag XOXO authentic hot chicken.",
+      },
+    },
+    {
+      id: 5,
+      name: "Christian Duffus",
+      lastName: "Duffus",
+      position: "Founder & CEO Fonbnk",
+      image: "/images/advisors/nft5.png",
+      about: {
+        name: "Christian Duffus",
+        title: "Pioneer Member of The Blockchain Research Institute",
+        body: "Kogi salvia cillum selvage small batch aliquip seitan DSA bitters. Ad chambray occaecat poutine esse humblebrag XOXO authentic hot chicken.",
+      },
+    },
+    {
+      id: 6,
+      name: "Kue Barinor Paul",
+      lastName: "Paul",
+      position: "Principal Partner, Blocklex.Tech",
+      image: "/images/advisors/nft6.png",
+      about: {
+        name: "Kue Barinor Paul",
+        title: "Pioneer Member of The Blockchain Research Institute",
+        body: "Kogi salvia cillum selvage small batch aliquip seitan DSA bitters. Ad chambray occaecat poutine esse humblebrag XOXO authentic hot chicken.",
+      },
+    },
+    {
+      id: 7,
+      name: "Kue Barinor Paul",
+      lastName: "Paul",
+      position: "Principal Partner, Blocklex.Tech",
+      image: "/images/advisors/nft7.png",
+      about: {
+        name: "Kue Barinor Paul",
+        title: "Pioneer Member of The Blockchain Research Institute",
+        body: "Kogi salvia cillum selvage small batch aliquip seitan DSA bitters. Ad chambray occaecat poutine esse humblebrag XOXO authentic hot chicken.",
+      },
+    },
+    {
+      id: 8,
+      name: "Kue Barinor Paul",
+      lastName: "Paul",
+      position: "Principal Partner, Blocklex.Tech",
+      image: "/images/advisors/nft8.png",
+      about: {
+        name: "Kue Barinor Paul",
+        title: "Pioneer Member of The Blockchain Research Institute",
+        body: "Kogi salvia cillum selvage small batch aliquip seitan DSA bitters. Ad chambray occaecat poutine esse humblebrag XOXO authentic hot chicken.",
+      },
+    },
+  ];
+
+  return { nftContent, bgContent, showCaseContent };
+}
